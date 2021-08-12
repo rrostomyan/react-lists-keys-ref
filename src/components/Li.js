c7d2e72936471f103ddff9bb251f45b89ffbfc0e
rrostomyan <robert.rostomyan@gmail.com>
2021-08-12 19:15:05 +0400

    removed componentDidMount

--- src/components/Li.js
+++ src/components/Li.js
@@ -1,9 +1,6 @@
 import React from "react";
 
 class Li extends React.Component {
-    componentDidMount() {
-        console.log('li did mount');
-    }
 
     componentWillUnmount() {
         console.log('li tag did unmount');
