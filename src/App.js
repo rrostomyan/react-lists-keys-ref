fff0668681634cbb4b4b5ed35002d4cd528192a7
Sergey <sergeyasatryan123@gmail.com>
2022-03-24 14:42:54 +0400

    update react version

--- src/App.js
+++ src/App.js
@@ -1,65 +1,8 @@
-import React from 'react';
-import './App.css';
-import Li from './components/Li';
-import Button from './components/Button';
+import React from "react";
+import "./App.css";
 
-class App extends React.Component {
-  state = {
-    items: [1, 2, 3],
-  };
-  constructor(props) {
-    super(props);
-    this.ref = React.createRef();
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick() {
-    alert(`App did clicked`);
-  }
-
-  showRefExample = () => {
-    this.ref.current.handleBtnClick();
-  };
-  componentDidMount() {
-    alert('component did mount');
-    alert('Did mount!');
-    alert('new change');
-
-    this.timer = setInterval(() => {
-      this.setState({
-        items: [
-          Math.floor(Math.random() * 1000),
-          Math.floor(Math.random() * 1000),
-          Math.floor(Math.random() * 1000),
-        ],
-      });
-    }, 2000);
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log('App did update');
-  }
-
-  componentWillUnmount() {
-    console.log('App did unmount');
-    if (this.timer) clearInterval(this.timer);
-  }
-
-  render() {
-    const { items } = this.state;
-    return (
-      <div>
-        <ul>
-          {items.map((item, index) => (
-            <Li key={item} item={item} />
-          ))}
-        </ul>
-        <button onClick={this.showRefExample}>Show ref example</button>
-        <Button handleParentClick={this.handleClick} name="click me 1" />
-        <Button ref={this.ref} name="click me 2" />
-      </div>
-    );
-  }
+function App() {
+  return <div>Empty App</div>;
 }
 
 export default App;
