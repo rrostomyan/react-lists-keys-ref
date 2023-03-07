526e37fb2a8fe23592c9a01c211f4b5e6b2943c1
Gregori Matinyan <gudzon437@gmail.com>
2023-03-07 19:13:59 +0400

    changed button text

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ function App() {
     <>
       <div>Empty App for test user</div>
       <TextField value="gdfdfg" />
-      <Button variant="contained">Click</Button>
+      <Button variant="contained">Increment</Button>
 
       <div>from Artur</div>
       <div>hello</div>
-
     </>
   );
 }
